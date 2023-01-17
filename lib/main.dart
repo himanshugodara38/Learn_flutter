d608452d486c5849a74796db989cd2e97c09027e
Bunty <buntygodara38@gmail.com>
2023-01-17 13:18:25 +0530

    day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:first_app/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -8,15 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('My first App'),
-        ),
-        body: const Center(
-          child: Text('Hello Guys'),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
